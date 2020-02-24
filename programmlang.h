ff1c3478082fbb09343f8a54b89c8d1055ee204b
blackerking <2019363+blackerking@users.noreply.github.com>
2020-02-24 11:36:45 +0100

    Delete programmlang.h

--- programmlang.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROGRAMMLANG_H_INCLUDED
-#define PROGRAMMLANG_H_INCLUDED
-
-std::string msgfehlertext = " - Fehler bei der Installation!\n\nFehlercode: ";
-std::string frage = " Installieren? ";
-#endif // PROGRAMMLANG_H_INCLUDED
